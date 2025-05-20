20c684527b06bcf2b3e0fb95af2cbea65da3d1be
TinToSer <52107530+TinToSer@users.noreply.github.com>
2025-05-20 15:16:37 +0200

    Update Pscad_License_Control.py

--- Pscad_License_Control.py
+++ Pscad_License_Control.py
@@ -5,7 +5,54 @@
 
 import requests
 from bs4 import BeautifulSoup
+from openpyxl import Workbook
+import json
+import datetime
+from openpyxl.styles import Font, Border, Side
 
+
+def write_to_excel(lic_db,excel_file):
+    wb = Workbook()
+    ws_lic = wb.active
+    ws_lic.title = "Users"
+    wb.create_sheet(title="Overview")
+    ws_overview = wb["Overview"]
+    users_header_required = ["License Group","License Feature","License ID","Checked out BY","Machine","Expiry","Checkout Expiry","Checkout Period"]
+    overview_header_required = ["License Group","License Feature","License Count","License Checkouts"]
+    ws_lic.append(users_header_required)
+    ws_overview.append(overview_header_required)
+
+
+    for each_group_data in lic_db["Groups"].values():
+        ws_overview.append(list(each_group_data.values())[:-1])
+        for each_member in each_group_data["Members"]:
+            row = list(each_member.values())
+            ws_lic.append(row)
+    
+    for ws_n in wb.sheetnames:
+        ws=wb[ws_n]
+        for col in ws.columns:
+            max_length = 0
+            for cell in col:
+                try:
+                    if cell.value:
+                        max_length = max(max_length, len(str(cell.value)))
+                except:
+                    pass
+            ws.column_dimensions[col[0].column_letter].width = max_length + 10
+        
+        for cell in ws[1]:
+            cell.font = Font(bold=True, size=14)
+            cell.border = Border(
+                            left=Side(style='thick'),
+                            right=Side(style='thick'),
+                            top=Side(style='thick'),
+                            bottom=Side(style='thick')
+                        )
+        ws.auto_filter.ref = ws.dimensions
+
+    wb.save(f"{datetime.datetime.now().strftime("%d%m%Y_%H%M%S")}_{excel_file}")
+        
 def get_pscad_license_usage(username, password):
     session = requests.session()
     headers = {
@@ -38,6 +85,7 @@ def get_pscad_license_usage(username, password):
         html_data = json_data.get("layout")
         soup = BeautifulSoup(html_data, 'html.parser')
         workgroups = soup.find_all('div', class_='cardLineItem')
+        search_db = {}
         for wg in workgroups:
             name = wg.find('div', class_='columnWorkgroupsName').text.strip()
             creation = wg.find('div', class_='columnWorkgroupsCreation').text.strip()
@@ -46,11 +94,12 @@ def get_pscad_license_usage(username, password):
             members = wg.find('div', class_='columnWorkgroupsMembers').text.strip()
             licenses = wg.find('div', class_='columnWorkgroupsLicenses').text.strip()
             join_codes = wg.find('div', class_='columnWorkgroupsJoinCodes').text.strip()
-            lic_db[name] = {"Id":wg.get("id"),"Created": creation, "Expiration": expiration,
+            search_db[name] = {"Id":wg.get("id"),"Created": creation, "Expiration": expiration,
                 "Maintenance Expiry": maintenance, "Members_count": members,"Members":[], "Licenses": licenses, "Join Codes": join_codes}
+            lic_db=search_db.copy()
         url = "https://mycentre.pscad.com/logics/ajax/workgroupMember/ajx-license-view.php"
 
-        for each_pscad in lic_db:
+        for each_pscad in search_db:
             files = {
                 "workgroupID": (None, lic_db[each_pscad].get("Id"))
             }
@@ -59,8 +108,20 @@ def get_pscad_license_usage(username, password):
             html_data = json_data.get("layout")
             soup = BeautifulSoup(html_data, 'html.parser')
             license_groups = soup.find_all('div', class_='cardLineMulti')
-
+            lic_db["Groups"] = {}
             for group in license_groups:
+                lic_group = group.find('div', class_='columnLicensesGroupName').text.strip()
+                lic_feature = group.find('div', class_='columnLicensesGroupFeatures').text.strip()
+                lic_count = group.find('div', class_='columnLicensesGroupCount').text.strip()
+                lic_checkouts = group.find('div', class_='columnLicensesGroupCheckout').text.strip()
+                each_inst = f"{lic_group} {lic_feature}"
+                lic_db["Groups"][each_inst]={
+                    "License Group":lic_group,
+                    "License feature":lic_feature,
+                    "License count":lic_count,
+                    "License Checkouts":lic_checkouts,
+                    "Members":[]
+                }
                 detail_items = group.find_all('div', class_='cardLineTwoInfoLicense')
                 for detail in detail_items:
                     license_id = detail.find('div', class_='columnLicensesID').text.strip()
@@ -71,22 +132,25 @@ def get_pscad_license_usage(username, password):
                     checkout_expiry = detail.find('div', class_='columnLicensesCheckoutExpiry').text.strip()
                     checkout_period = detail.find('div', class_='columnLicensesCheckoutPeriod').text.strip()
 
-                    lic_db[each_pscad]["Members"].append({
-                        'license_id': license_id,
-                        'name': license_name,
-                        'expiry': expiry,
-                        'machine': machine,
-                        'checked_out_by': checked_out_by,
-                        'checkout_expiry': checkout_expiry,
-                        'checkout_period': checkout_period
+                    lic_db["Groups"][each_inst]["Members"].append({
+                        'License Group': license_name,
+                        'License Feature':lic_feature,
+                        'License ID': license_id,
+                        'Checked out BY': checked_out_by,
+                        'Machine': machine,
+                        'Expiry': expiry,
+                        'Checkout Expiry': checkout_expiry,
+                        'Checkout Period': checkout_period
                     })
     return lic_db
 
 if __name__ == "__main__":
-    username = "your_username"
-    password = "your_password"
+    username = ""
+    password = ""
+    json_file = "license_users.json"
+    excel_file = "license_users.xlsx"
     lic_db = get_pscad_license_usage(username, password)
-    print(lic_db)
 
-
-
+    write_to_excel(lic_db,excel_file)
+    json.dump(lic_db,open(f"{datetime.datetime.now().strftime("%d%m%Y_%H%M%S")}_{json_file}","w+"),indent=4)
+    
